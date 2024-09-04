99a3b292a607ee964b4465e7ab9229645a87a757
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2024-09-04 19:51:18 -0300

    float button component

---
+++ src/components/FloatButton/index.tsx
@@ -0,0 +1,19 @@
+import { colors } from "@/styles/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import { FloatButtonProps } from "./types";
+
+export function FloatButton({ icon, ...rest }: FloatButtonProps) {
+  return (
+    <TouchableOpacity 
+      activeOpacity={0.7}
+      {...rest}
+    >
+      <View className="px-8 py-4 bg-gray-600 rounded-full absolute bottom-4 right-3 flex-row gap-2 items-center">
+        <MaterialIcons name={icon} size={22} color={colors.orange[500]} />
+        <Text className="font-subtitle text-orange-500">Escrever</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
