a19f058d00d5302d238459f9030732b9ac5cba21
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2024-09-04 17:24:47 -0300

    import roboto fonts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Home</Text>
+    <View className="flex-1 bg-gray-900 justify-center items-center">
+      <Text className="">Home</Text>
     </View>
   );
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,33 @@
 import "@/styles/global.css";
 
-import { Slot } from "expo-router";
+import { Slot, SplashScreen } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto"
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, fontsError] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
+
+  if(!fontsLoaded && !fontsLoaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
       <Slot />
     </GestureHandlerRootView>
   );
