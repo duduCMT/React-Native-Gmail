7034c3777dfc5d61d7ece407586ae8afe8d8ebc0
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2024-09-06 15:36:23 -0300

    open drawer when press search menu button

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -1,10 +1,19 @@
 import { Pressable, View } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
+import { MenuButtonProps } from "./types";
+import { useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
 
-export function MenuButton() {
+export function MenuButton({ ...rest }: MenuButtonProps) {
+  const navigation = useNavigation();
+  
+  const toggleMenu = () => {
+    navigation.dispatch(DrawerActions.toggleDrawer());
+  };
+
   return (
-    <Pressable>
+    <Pressable onPress={toggleMenu} {...rest}>
       <MaterialIcons name="menu" size={22} color={colors.white} />
     </Pressable>
   )
